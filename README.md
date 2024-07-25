dd96bafc80250a3f836e43914885488a55e4fcee
karkiamrit <amrit.karki2073@gmail.com>
2024-07-25 21:07:38 +0545

    initial setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# tdd_tutorials
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/dbstech/tdd_tutorials/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.dbstech.tdd_tutorials
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
